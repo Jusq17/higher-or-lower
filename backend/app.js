0deb0b422bbeef8764a8e711fd209497f720a1a7
Juska <juseljus@gmail.com>
2025-03-16 22:32:40 +0200

    port added to .env

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,8 @@ app.use('/api/register', registerRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/user', userRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
